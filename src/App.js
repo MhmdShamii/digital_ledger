61e4be53659eb800661a30c21f5d7fa376a30a4e
Mhmd Shami <mhmddshamii@gmail.com>
2025-11-23 16:17:06 +0200

    deployment setup

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/digital-ledger">
       <div className="min-h-screen w-full bg-gray-200 flex flex-col items-center p-4 box-border">
         <NavBar />
         <Routes>
